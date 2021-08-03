d98fcec28c589a7b9bb8b115646ab58ae2a3508a
dongwu-kim <alduk7797@naver.com>
2021-08-03 13:53:48 +0900

    [MODIFY] : conflict 대비 수정

--- src/Routes.js
+++ src/Routes.js
@@ -3,8 +3,6 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Nav from './component/Nav/Nav';
 import Footer from './component/Footer/Footer';
 import Main from './pages/Main/Main';
-import Login from './pages/Login/Login';
-import SignUp from './pages/SignUp/SignUp';
 
 class Routes extends React.Component {
   render() {
@@ -13,8 +11,6 @@ class Routes extends React.Component {
         <Nav />
         <Switch>
           <Route exact path="/" component={Main} />
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/signup" component={SignUp} />
         </Switch>
         <Footer />
       </Router>
