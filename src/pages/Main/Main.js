04c9067ebd7a86fad0317896d274ee50e4f8c35c
dongwu-kim <alduk7797@naver.com>
2021-08-04 21:57:20 +0900

    main_layout setting

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -19,7 +19,40 @@ class Main extends React.Component {
             <button className="moveButton leftButton">{`<`}</button>
             <button className="moveButton rightButton">{`>`}</button>
           </div>
-          <div className="tasteRecommend"></div>
+          <div className="tasteRecommendWrap">
+            <div className="tasteListBlock">
+              <button>
+                이국적인맛 <i className="fas fa-chevron-down"></i>
+              </button>
+              <span>메뉴</span>
+              <span>추천드려요</span>
+              <p>
+                로그인하시면 고객님의 구매내역과 맛취향에 따라 딱맞는 맛있는
+                메뉴를 추천드려요
+              </p>
+            </div>
+            <div className="imgSlider">
+              <div className="imgBlock">
+                <img
+                  alt="stake"
+                  src="/images/Main/stake.jpg"
+                  width="520px"
+                  height="520px"
+                />
+                <div className="recommendImgDescription">
+                  <div className="productTitle">
+                    <span>쩌는 스테끼</span>
+                  </div>
+                  <div className="productDescription">
+                    <span className="productPrice">10억원</span>
+                    <span>1인분</span>
+                    <i className="fas fa-cart-plus"></i>
+                  </div>
+                </div>
+              </div>
+              <div className="shader"></div>
+            </div>
+          </div>
           <div className="imgSlider"></div>
           <div className="onTimeBest"></div>
           <div className="onTimeReview"></div>
@@ -28,6 +61,7 @@ class Main extends React.Component {
             <div className="pdRightWrap"></div>
           </div>
         </div>
+        <div className="mainTimer"></div>
       </section>
     );
   }
